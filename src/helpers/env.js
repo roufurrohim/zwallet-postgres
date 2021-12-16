b95a4ddd60b3a580f63e2154972eac6f1678caa5
mbr <mbr@mbr.kali>
2021-12-16 15:54:17 +0700

    change db host env

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -4,7 +4,7 @@ const env = {
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   JWT_SECRET: process.env.JWT_SECRET,
-  DB_HOST: process.env.DB_HOST,
+  DB_HOST: process.env.DATABASE_URL,
   DB_NAME: process.env.DB_NAME,
 };
 module.exports = env;
